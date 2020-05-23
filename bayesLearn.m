03cb9382209a9953e2e0481ea36c12b8dcfd3922
ozan-san <ozan.san.98@gmail.com>
2020-05-23 23:38:18 +0300

    Initial Commit of given material
    (Data provided by EyeDea Recognition s.r.o.)

---
+++ bayesLearn.m
@@ -0,0 +1,12 @@
+function [ bayes ] = bayesLearn( train,train_labels )
+%bayesLearn naive bayes classifier learning
+%   [ bayes ] = bayesLearn( train,train_labels )
+% train - matrix with training examples in rows
+% train_labels - column with labels of the training examples (char array)
+% bayes - structure with the learned classifier
+
+%          TODO Here you have to implement your function for training naive bayes
+%          classifier
+
+end
+
---
+++ nnLearn.m
@@ -0,0 +1,20 @@
+function [ nn ] = nnLearn( train,trainLabels )
+%nnLearn 1-nearest neighbour learning
+%   [ nn ] = nnLearn( train,trainLabels )
+% train - matrix with training examples in rows
+% trainLabels - column with labels of the training examples (char array)
+% nn - structure with the learned classifier
+
+%         TODO Here you have to implement your function for training 1-nearest
+%         neighbor classifier
+
+% we can speed up the process by dividing each element of
+% the matrices of luminances by 16, because we do not
+% differentiate that much between luminances that are
+% close
+
+
+
+
+end
+
